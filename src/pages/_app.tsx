dd63ba7e9b1cc29ba0fc8dfd5c4af0791d188715
Abbas Khan <abbasgkhan@gmail.com>
2023-03-15 16:39:00 -0400

    Your commit message here

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '<fateh>/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import type { AppProps } from 'next/app';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {
+      backgroundColor: {
+        'primary': '#1F2937',
+        'secondary': '#4B5563',
+        'accent': '#0EA5E9',
+      },
+      textColor: {
+        'primary': '#1F2937',
+        'secondary': '#4B5563',
+        'accent': '#0EA5E9',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
